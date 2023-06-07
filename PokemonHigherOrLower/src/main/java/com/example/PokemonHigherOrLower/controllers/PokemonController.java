e1ece00e19377267d032a36c3e0ed03efaf479e3
Umar Twynam <umartwynam@gmail.com>
2023-06-07 15:52:35 +0100

    Revert "tried to get RestTemplate to work"
    This reverts commit 24bfcef97bd88ae1fc9bc7d4b41e026ac51c9897.

--- PokemonHigherOrLower/src/main/java/com/example/PokemonHigherOrLower/controllers/PokemonController.java
+++ PokemonHigherOrLower/src/main/java/com/example/PokemonHigherOrLower/controllers/PokemonController.java
@@ -1,14 +1,9 @@
 package com.example.PokemonHigherOrLower.controllers;
 
-import com.example.PokemonHigherOrLower.models.Pokemon;
 import com.example.PokemonHigherOrLower.services.PokemonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/pokemon")
@@ -16,10 +11,4 @@ public class PokemonController {
 
     @Autowired
     PokemonService pokemonService;
-
-    @GetMapping(value ="/{id}")
-    public ResponseEntity<List<Pokemon>> comparePokemon(){
-        List<Pokemon>pokemonList = pokemonService.getTwoRandomPokemon();
-        return ResponseEntity.ok(pokemonList);
-    }
 }
