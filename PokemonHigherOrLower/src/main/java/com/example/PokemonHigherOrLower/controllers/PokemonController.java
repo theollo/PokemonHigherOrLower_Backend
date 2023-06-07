24bfcef97bd88ae1fc9bc7d4b41e026ac51c9897
Theo Lloyd <theolloyd@Theos-MacBook-Air.local>
2023-06-07 15:16:15 +0100

    tried to get RestTemplate to work

--- PokemonHigherOrLower/src/main/java/com/example/PokemonHigherOrLower/controllers/PokemonController.java
+++ PokemonHigherOrLower/src/main/java/com/example/PokemonHigherOrLower/controllers/PokemonController.java
@@ -1,9 +1,14 @@
 package com.example.PokemonHigherOrLower.controllers;
 
+import com.example.PokemonHigherOrLower.models.Pokemon;
 import com.example.PokemonHigherOrLower.services.PokemonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/pokemon")
@@ -11,4 +16,10 @@ public class PokemonController {
 
     @Autowired
     PokemonService pokemonService;
+
+    @GetMapping(value ="/{id}")
+    public ResponseEntity<List<Pokemon>> comparePokemon(){
+        List<Pokemon>pokemonList = pokemonService.getTwoRandomPokemon();
+        return ResponseEntity.ok(pokemonList);
+    }
 }
--- PokemonHigherOrLower/src/main/java/com/example/PokemonHigherOrLower/services/PokemonService.java
+++ PokemonHigherOrLower/src/main/java/com/example/PokemonHigherOrLower/services/PokemonService.java
@@ -2,26 +2,57 @@ package com.example.PokemonHigherOrLower.services;
 
 import com.example.PokemonHigherOrLower.models.Pokemon;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class PokemonService {
     private String apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+    private RestTemplate restTemplate;
+
+    public PokemonService(RestTemplate restTemplate){
+        this.restTemplate = restTemplate;
+    }
 
     int id1= getRandomPokemonId();
     int id2= getRandomPokemonId();
 
+    public List<Pokemon> getTwoRandomPokemon() {
+        List<Pokemon> pokemonList = new ArrayList<>();
+
+        Pokemon pokemon1 = getPokemonById(id1);
+        Pokemon pokemon2 = getPokemonById(id2);
+
+        pokemonList.add(pokemon1);
+        pokemonList.add(pokemon2);
+        return pokemonList;
+    }
+
     private int getRandomPokemonId(){
         return ThreadLocalRandom.current().nextInt(1,1010);
     }
-    Random r = new Random();
-    int low = 10;
-    int high = 1010;
-    int result = r.nextInt(high-low) + low;
+//    Random randomId = new Random(){
+//    int low = 1;
+//    int high = 1010;
+//    int result = randomId.nextInt(high - low) + low;
+
 
     private Pokemon getPokemonById(int id){
         String url = apiUrl + id;
+        Pokemon pokemon = restTemplate.getForObject(url, Pokemon.class);
+    if (pokemon != null){
+        int totalPower = calculateTotalPower(pokemon);
+        pokemon.setTotalPower(totalPower);
+    }
+    return pokemon;
+    }
+
+    private int calculateTotalPower(Pokemon pokemon){
+        int totalPower = 0;
+        int
     }
 }
