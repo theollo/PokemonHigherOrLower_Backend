e0486ebb6880f9e21dbbd0f98e402c5662d62d19
Theo Lloyd <theolloyd@Theos-MacBook-Air.local>
2023-06-08 15:50:27 +0100

    added game controller

---
+++ PokemonHigherOrLower/src/main/java/com/example/PokemonHigherOrLower/controllers/GameController.java
@@ -0,0 +1,34 @@
+package com.example.PokemonHigherOrLower.controllers;
+
+import com.example.PokemonHigherOrLower.services.PlayerService;
+import com.example.PokemonHigherOrLower.services.PokemonService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/game")
+public class GameController {
+    private final PlayerService playerService;
+    private final PokemonService pokemonService;
+
+    public GameController(PlayerService playerService, PokemonService pokemonService) {
+        this.playerService = playerService;
+        this.pokemonService = pokemonService;
+    }
+
+    // constructor, inject services
+
+    @PostMapping("/start")
+    public void startGame(@RequestBody String name) {
+        playerService.createPlayer(name);
+    }
+
+    @PostMapping("/guess/{pokemonId}")
+    public void makeGuess(@PathVariable int pokemonId) {
+        // similar to previous makeGuess, you should compare pokemonId with the one with the highest total power
+    }
+
+    @PostMapping("/reset")
+    public void resetGame() {
+        playerService.resetPlayer();
+    }
+}
--- PokemonHigherOrLower/src/main/java/com/example/PokemonHigherOrLower/services/PlayerService.java
+++ PokemonHigherOrLower/src/main/java/com/example/PokemonHigherOrLower/services/PlayerService.java
@@ -1,4 +1,21 @@
 package com.example.PokemonHigherOrLower.services;
 
+import com.example.PokemonHigherOrLower.models.Player;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PlayerService {
+    private Player player;
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void createPlayer(String name) {
+        player = new Player(name);
+    }
+
+    public void resetPlayer() {
+        player = null;
+    }
 }
