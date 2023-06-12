d056325c8b922cdb31319f6b90b96d5c5f0e0a0c
Umar Twynam <umartwynam@gmail.com>
2023-06-12 14:53:10 +0100

    added score, isComplete, and player to Game model

--- PokemonHigherOrLower/src/main/java/com/example/PokemonHigherOrLower/models/Game.java
+++ PokemonHigherOrLower/src/main/java/com/example/PokemonHigherOrLower/models/Game.java
@@ -1,21 +1,38 @@
 package com.example.PokemonHigherOrLower.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.beans.ConstructorProperties;
 
 @Entity(name="games")
 public class Game {
     @Id
     private Long id;
 
+    @Column
+    private int score;
+
+    @Column
+    private boolean isComplete;
+
+//    @ManyToOne
+//    @JoinColumn(name="player")
+//    private Player player;
+
+    @JsonIgnoreProperties({"games"})
     @ManyToOne
-    @JoinColumn(name="player")
+    @JoinColumn(name = "player_id", nullable = false)
     private Player player;
 
+    // Constructor
     public Game(Player player) {
+        this.score = 0;
+        this.isComplete = false;
+        this.player = player;
     }
+
+    // empty constructor
     public Game(){
 
     }
